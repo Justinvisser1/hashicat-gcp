31a826d74d909d118f02479c3cbf86251d18c629
Justin Visser <justin.visser1@gfs.com>
2021-07-14 19:48:33 +0000

    Added the second label

--- main.tf
+++ main.tf
@@ -45,10 +45,10 @@ resource "tls_private_key" "ssh-key" {
 
 resource "google_compute_instance" "hashicat" {
   department   = "devops"
+  billable     = "true"
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-
 
   boot_disk {
     initialize_params {
